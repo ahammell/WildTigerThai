80b1819d064f112c8a44b8c85c5d083adba3f1f4
Nathan Geranis <nathangeranis@outlook.com>
2019-02-28 23:40:56 -0600

    V-1.0 MVP

--- WildTigerThai/WildTigerThai.UI/Controllers/AccountController.cs
+++ WildTigerThai/WildTigerThai.UI/Controllers/AccountController.cs
@@ -153,11 +153,8 @@ namespace WildTigerThai.UI.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
-                    ViewBag.Link = callbackUrl;
-                    return View("DisplayEmail");
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
